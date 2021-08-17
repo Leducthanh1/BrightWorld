45ccfcd817302d642d41a2fde88334af71a418ea
Leducthanh1 <ronthanhyal@gmail.com>
2021-08-17 09:45:31 +0700

    17-8-21

--- BrightWorld/App_Start/RouteConfig.cs
+++ BrightWorld/App_Start/RouteConfig.cs
@@ -12,11 +12,11 @@ namespace BrightWorld
         public static void RegisterRoutes(RouteCollection routes)
         {
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
-            routes.MapRoute(
-                name: "Admin",
-                url: "Admin/{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
-            );
+            //routes.MapRoute(
+            //    name: "Admin",
+            //    url: "Admin/{controller}/{action}/{id}",
+            //    defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+            //);
 
             routes.MapRoute(
                 name: "Default",
